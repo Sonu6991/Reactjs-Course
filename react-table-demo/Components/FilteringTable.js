57be3f8b453600dd1c64a0533a6c61d5b43acdb8
Sonu6991 <sonuchauhan8488@gmail.com>
2022-01-04 09:19:08 +0530

    react table demo

---
+++ react-table-demo/Components/FilteringTable.js
@@ -0,0 +1,136 @@
+import React from "react";
+import { useTable } from "react-table";
+import classes from "./Table.module.css";
+
+const FilteringTable = () => {
+  const data = React.useMemo(
+    () => [
+      {
+        firstName: "Sonu",
+        lastName: "Chauhan",
+        gender: "Female",
+        age: "21",
+        contact: "1234567890",
+        description:
+          "  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repudiandae suscipit blanditiis reprehenderit placeat ",
+      },
+      {
+        firstName: "Teajs",
+        lastName: "Patel",
+        gender: "Male",
+        age: "22",
+        contact: "2153486745",
+        description:
+          "amet consectetur adipisicing elit. Eaque explicabo magnam quod accusantium necessitatibus optio temporibus eligendi suscipit quo rerum",
+      },
+      {
+        firstName: "Ajay",
+        lastName: "Divaraniya",
+        gender: "Male",
+        age: "21",
+        contact: "3256987425",
+        description:
+          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque explicabo magnam quod accusantium necessitatibus optio temporibus eligendi suscipit quo rerum.",
+      },
+    ],
+    []
+  );
+  const columns = React.useMemo(
+    () => [
+      {
+        Header: "Name",
+        Footer: "Name",
+        columns: [
+          {
+            Header: "First name",
+            Footer: "First name",
+            accessor: "firstName", // accessor is the "key" in the data
+          },
+          {
+            Header: "Last name",
+            Footer: "Last name",
+            accessor: "lastName", // accessor is the "key" in the data
+          },
+        ],
+      },
+      {
+        Header: "Info",
+        Footer: "Info",
+        columns: [
+          {
+            Header: "Gender",
+            Footer: "Gender",
+            accessor: "gender", // accessor is the "key" in the data
+          },
+          {
+            Header: "Age",
+            Footer: "age",
+            accessor: "age", // accessor is the "key" in the data
+          },
+          {
+            Header: "Contact no",
+            Footer: "Contact no",
+            accessor: "contact", // accessor is the "key" in the data
+          },
+        ],
+      },
+      {
+        Header: "Description",
+        Footer: "Description",
+        accessor: "description",
+      },
+    ],
+    []
+  );
+  const table = useTable({ columns, data });
+
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    rows,
+    prepareRow,
+    footerGroups,
+  } = table;
+  return (
+    <div>
+      <h1>Table</h1>
+      <table className={classes.table} {...getTableProps()}>
+        <thead>
+          {headerGroups.map((headerGroup) => (
+            <tr {...headerGroup.getHeaderGroupProps()}>
+              {headerGroup.headers.map((column) => (
+                <th {...column.getHeaderProps()}>{column.render("Header")}</th>
+              ))}
+            </tr>
+          ))}
+        </thead>
+        <tbody {...getTableBodyProps()}>
+          {rows.map((row) => {
+            prepareRow(row);
+            return (
+              <tr {...row.getRowProps()}>
+                {row.cells.map((cell) => {
+                  return (
+                    <td {...cell.getCellProps()}>{cell.render("Cell")}</td>
+                  );
+                })}
+              </tr>
+            );
+          })}
+        </tbody>
+        {/* <tfoot>
+          {footerGroups.map((footergroup) => (
+            <tr {...footergroup.getFooterGroupProps()}>
+              {footergroup.headers.map((column) => (
+                <th {...column.getFooterProps()}>{column.render("Footer")}</th>
+              ))}
+            </tr>
+          ))}
+        </tfoot> */}
+      </table>
+    </div>
+  );
+};
+
+export default FilteringTable;
---
+++ react-table-demo/pages/index.js
@@ -0,0 +1,13 @@
+import Head from "next/head";
+import SortingTable from "../Components/SortingTable";
+
+import Tabel from "../Components/Tabel";
+
+export default function Home() {
+  return (
+    <div>
+      {/* <Tabel /> */}
+      <SortingTable />
+    </div>
+  );
+}
